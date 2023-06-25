8af2f08a4e31b1bf92141da50f6b38672914ec99
binael <nbinael@yahoo.com>
2023-06-25 18:26:42 +0100

    line by line replacement

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,17 @@
-file { '~etc/ssh/ssh_config':
-  ensure => present,
-  content => "Host *\n
-  	PasswordAuthentication no\n
-	IdentifyFile ~/.ssh/school"\n,
+# Replace or create lines for client configuration
+
+include stdlib
+
+file_line { 'password_change':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'identify_file':
+  ensure   => present,
+  path     => '/etc/ssh/ssh_config',
+  line     => '    IdentifyFile ~/.ssh/school',
+  replace  => true,
 }
