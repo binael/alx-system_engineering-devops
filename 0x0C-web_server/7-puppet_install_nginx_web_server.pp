372c53d513db5a0a8d327c6672408d2679584962
binael <nbinael@yahoo.com>
2023-06-27 15:54:28 +0100

    added puppet file

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+# Automates installation and configuration of nginx
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content =>
+"server {
+		listen 80 default_server;
+
+        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
+}"
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  hasstatus => true,
+  require   => Package['nginx'],
+}
